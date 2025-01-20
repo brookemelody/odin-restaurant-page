33438c48c6b855e132a40f12df29570159b4116e
Brooke <brookewu2023@gmail.com>
2025-01-19 20:30:07 -0500

    Set up loading CSS with webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import "./styles.css";
+
 alert("This website was set up using webpack!");
+
+// Inside div#content, create a homepage for your restaurant
+// Target the content div in template.html
+const contentDiv = document.querySelector("#content");
