0e2d983172408ca336b36f3d668f05bb31be83e3
Brooke <brookewu2023@gmail.com>
2025-01-19 20:43:16 -0500

    Set up loading images using webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import "./styles.css";
 
+import odinImage from "./odin.png";
+
 alert("This website was set up using webpack!");
 
 // Inside div#content, create a homepage for your restaurant
 // Target the content div in template.html
 const contentDiv = document.querySelector("#content");
+const image = document.createElement("img");
+image.src = odinImage;
+contentDiv.appendChild(image);
